783d7a11517d5062cb87cb89419a3ab6d756ac83
Petr <vankov.939@gmail.com>
2022-08-05 15:07:15 +0300

    some change

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 from transaq import TransaqConnector
 
 login = 'FZTC19829A'
-ip = '192.168.0.240'
+ip = 'localhost'
 def test():
     tConnector = TransaqConnector(ip)
-    tConnector.disconnect()
     if tConnector.connect(login):
         #tConnector.get_status()
         #tConnector.get_old_news()
-        print(tConnector.get_element_value('client'))
-        tConnector.get_servtime_difference()
-        print(tConnector.get_connector_version())
-        print(tConnector.get_server_id())
+        try:
+            print(tConnector.get_markets())
+            print(tConnector.get_securities_info('1', 'RU000A0JS603'))
+
+
+
+            
+        except Exception as e:
+            print(e)
+
+        # print(tConnector.get_element_value('client'))
+        # tConnector.get_servtime_difference()
+        # print(tConnector.get_connector_version())
+        # print(tConnector.get_server_id())
 
         tConnector.disconnect()
         tConnector.close()
