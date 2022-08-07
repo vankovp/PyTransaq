28cec7b3d8257a9fae6dcc676f3558461bc44137
petr <vankov.939@gmail.com>
2022-08-07 18:50:59 +0300

    fix

--- client.py
+++ client.py
@@ -156,7 +156,7 @@ class TData(Connector):
 
         try:
             while True:
-                self.recv(1)
+                str(self.recv(1), 'utf-8')
         except Exception as e:
             #print(e)
             return
