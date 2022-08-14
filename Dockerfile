1a9263be928410d8de6bc0aa64fac59ede34b6cb
petr <vankov.939@gmail.com>
2022-08-14 23:26:20 +0300

    Dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#FROM python:3.9.13-alpine3.16
+FROM amancevice/pandas:alpine
+RUN ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+WORKDIR /usr/src/app
+VOLUME /var/data
+COPY requirements.txt /usr/src/app/
+RUN pip install -r requirements.txt
+WORKDIR /usr/src/app
+VOLUME /var/data
+COPY *.py *.txt  ./
+RUN pip install -r requirements.txt
--- trader.py
+++ trader.py
@@ -2,6 +2,24 @@ from library import *
 
 from transaq import TransaqConnector
 
+
+
+def receive_quatations():
+    while True:
+        try:
+            update = tConnector.client.sub.receive_data(timeout=0.01)
+            if update is not None:
+                for inst_data in update.values():
+                    seccode = inst_data['seccode']
+                    if seccode not in quatations:
+                        quatations[seccode] = {}
+                    for key, value in inst_data.items():
+                        quatations[seccode][key] = value
+        except:
+            pass
+
+
+
 password = os.environ["TRANSAQ_PASS"]
 login = os.environ["TRANSAQ_LOGIN"]
 adapter_address = os.environ["TRANSAQ_ADAPTER_ADDRESS"]
@@ -19,3 +37,53 @@ logger = setup_logger("logger1", logs_path)
 logger.setLevel(logging.INFO)
 
 logger.info("Initialisation")
+
+
+logger.info('loading tinkoff tickers')
+
+tinkoff_tickers = []
+
+with open('tinkoff_tickers.txt') as f:
+    lines = f.readlines()
+for line in lines:
+    tinkoff_tickers.append(line[:-1])
+
+
+
+logger.info('%s tinkoff tickers to exclude', len(tinkoff_tickers))
+
+logger.info('connecting to transaq adapter')
+
+tConnector = TransaqConnector(adapter_address)
+tConnector.connect(login)
+
+
+logger.info('Connection established')
+
+logger.info('getting portfolio')
+
+portfolio = get_portfolio(tConnector)
+
+logger.info("%s bonds in portfolio", str(len(portfolio['positions'])))
+
+logger.info ("getting bonds df")
+
+bonds_df = get_bonds_df(tConnector, logger)
+
+logger.info('bonds_df obtained')
+
+logger.info('selecting bonds to buy')
+
+selected_by_price, boards = select_bonds_by_price(bonds_df, except_list = tinkoff_tickers)
+
+logger.info("%s bonds are selected to buy", str(len(selected_by_price)))
+
+logger.info("subscribing on selected bonds quataions")
+
+
+tConnector.subscribe(method = 'quotations', seccodes = selected_by_price,
+                    boards = boards, callback = None)
+
+logger.info('subscribed')
+
+receive_quatations()
